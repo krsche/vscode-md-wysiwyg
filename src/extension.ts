50aa9fe53dde1ef761f37bf7df4d18ec29ac0f18
Fabian Kirschner <fabian.kirschner@gmail.com>
2021-09-24 21:53:36 +0200

    added some demo commands and info-messages

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,26 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "md-wysiwyg" is now active!');
+    console.log('Congratulations, your extension "md-wysiwyg" is now active!');
 
-	let disposable = vscode.commands.registerCommand('md-wysiwyg.helloWorld', () => {
-		vscode.window.showInformationMessage('Moin Meister!');
-	});
-
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(
+        vscode.commands.registerCommand('md-wysiwyg.helloWorld', () => {
+            vscode.window.showInformationMessage('Moin Meister!');
+        })	
+    );
+    context.subscriptions.push(
+        vscode.commands.registerCommand('md-wysiwyg.moin', async () => {
+            let answer = await vscode.window.showInformationMessage('Moin Meister! How r things?', 'Mega', 'Even mega-er');
+            
+            if (answer === "Mega") { 
+                vscode.window.showInformationMessage("Fett!"); 
+            } else if (answer === "Even mega-er") { 
+                vscode.window.showInformationMessage("Richtig Fett!");
+            } else { 
+                console.log('Macht der einfach zu....');
+            }
+        })	
+    );
 }
 
 export function deactivate() { }
